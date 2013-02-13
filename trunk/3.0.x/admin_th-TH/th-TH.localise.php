8c720abf2c1dfdd633cd0fedac8ea8d7586ce609
DOCManLaiThai <supachai_chai@hotmail.com>
2013-02-13 11:46:53 +0700

    # Update prepare 3.0.3v1 lang packs.
    # Update prepare 3.0.3v1 lang packs.

--- trunk/3.0.x/admin_th-TH/th-TH.localise.php
+++ trunk/3.0.x/admin_th-TH/th-TH.localise.php
@@ -2,7 +2,7 @@
 /**
  * @package    Joomla.Language
  *
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -20,26 +20,33 @@ abstract class th_THLocalise
 	 * Returns the potential suffixes for a specific number of items
 	 *
 	 * @param	int $count  The number of items.
+	 *
 	 * @return	array  An array of potential suffixes.
+	 *
 	 * @since	1.6
 	 */
 	public static function getPluralSuffixes($count)
 	{
-		if ($count == 0) {
+		if ($count == 0)
+		{
 			$return =  array('0');
 		}
-		elseif($count == 1) {
+		elseif ($count == 1)
+		{
 			$return =  array('1');
 		}
-		else {
+		else
+		{
 			$return = array('MORE');
 		}
 		return $return;
 	}
+
 	/**
 	 * Returns the ignored search words
 	 *
 	 * @return	array  An array of ignored search words.
+	 *
 	 * @since	1.6
 	 */
 	public static function getIgnoredSearchWords()
@@ -50,30 +57,36 @@ abstract class th_THLocalise
 		$search_ignore[] = "on";
 		return $search_ignore;
 	}
+
 	/**
 	 * Returns the lower length limit of search words
 	 *
 	 * @return	integer  The lower length limit of search words.
+	 *
 	 * @since	1.6
 	 */
 	public static function getLowerLimitSearchWord()
 	{
 		return 3;
 	}
+
 	/**
 	 * Returns the upper length limit of search words
 	 *
 	 * @return	integer  The upper length limit of search words.
+	 *
 	 * @since	1.6
 	 */
 	public static function getUpperLimitSearchWord()
 	{
 		return 20;
 	}
+
 	/**
 	 * Returns the number of chars to display when searching
 	 *
 	 * @return	integer  The number of chars to display when searching.
+	 *
 	 * @since	1.6
 	 */
 	public static function getSearchDisplayedCharactersNumber()
