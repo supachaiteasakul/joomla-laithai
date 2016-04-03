5765d4386e36cdb625222e2f0d92a2a9fcaefaca
supachai_chai@hotmail.com <supachai_chai@hotmail.com>
2016-04-04 02:01:28 +0700

    # Updated translation after 3.5.0 Stable Release
    # Updated translation after 3.5.0 Stable Release

--- trunk/3.5.x/site_th-TH/th-TH.localise.php
+++ trunk/3.5.x/site_th-TH/th-TH.localise.php
@@ -2,7 +2,7 @@
 /**
  * @package    Joomla.Language
  *
- * @copyright	Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license	GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -28,17 +28,16 @@ abstract class Th_THLocalise
 	{
 		if ($count == 0)
 		{
-			$return =  array('0');
+			return array('0');
 		}
 		elseif ($count == 1)
 		{
-			$return =  array('1');
+			return array('1');
 		}
 		else
 		{
-			$return = array('MORE');
+			return array('MORE');
 		}
-		return $return;
 	}
 
 	/**
@@ -50,11 +49,7 @@ abstract class Th_THLocalise
 	 */
 	public static function getIgnoredSearchWords()
 	{
-		$search_ignore = array();
-		$search_ignore[] = "and";
-		$search_ignore[] = "in";
-		$search_ignore[] = "on";
-		return $search_ignore;
+		return array('and', 'in', 'on');
 	}
 
 	/**
