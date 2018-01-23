90d121eca771269cdd42d16904671f4e4afea84c
Supachai Teasakul <supachai_chai@hotmail.com>
2018-01-23 12:15:11 +0700

    Update site language for 3.8.4

--- trunk/3.8.x/site_th-TH/th-TH.localise.php
+++ trunk/3.8.x/site_th-TH/th-TH.localise.php
@@ -2,8 +2,8 @@
 /**
  * @package    Joomla.Language
  *
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license	GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
